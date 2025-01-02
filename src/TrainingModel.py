b48dadd8cf8128d9f6237d8af81cbfd5a1cad9ec
Connor McKenzie <connormckenzie@mysite.local>
2025-01-02 16:00:37 +0000

    Added ability to load saved model or train a new model

--- src/TrainingModel.py
+++ src/TrainingModel.py
@@ -1,3 +1,5 @@
+import pickle
+
 import joblib
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
@@ -7,11 +9,11 @@ from sklearn.preprocessing import LabelEncoder
 
 from src.utils.utils import get_file_path
 
+model_file_path = get_file_path("./models/race_prediction_model.pkl")
 
 class TrainingModel:
 
-    def __init__(self, df):
-        self.model = RandomForestClassifier(n_estimators=100)
+    def __init__(self, df, train_new_model):
         self.label_encoder = LabelEncoder()
 
         self.df = df
@@ -23,7 +25,12 @@ class TrainingModel:
         self.Y = df["track_constructor_position"]
         self.Y = self.Y.fillna(0)
 
-        self.train_model()
+        if train_new_model:
+            self.model = RandomForestClassifier(n_estimators=100)
+            self.train_model()
+            self.save_model()
+        else:
+            self.model = pickle.load(open(model_file_path, 'rb'))
 
 
     def prepare_X(self):
@@ -44,7 +51,6 @@ class TrainingModel:
         X_train, X_test, Y_train, Y_test, rounds_train, rounds_test = train_test_split(self.X, self.Y, self.rounds, test_size=0.2, random_state=42)
 
         self.model.fit(X_train, Y_train)
-        joblib.dump(self.model, get_file_path("./models/race_prediction_model.pkl"))
 
         Y_pred = self.model.predict(X_test)
         results = rounds_test.copy()
@@ -53,3 +59,8 @@ class TrainingModel:
 
         accuracy = accuracy_score(Y_test, Y_pred)
         print("Accuracy:", accuracy)
+
+
+    def save_model(self):
+        with open(model_file_path, 'wb') as file:
+            pickle.dump(self.model, file)
